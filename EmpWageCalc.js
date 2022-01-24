16063eaa55cd823143db668b19217a748ccfac93
rksgr <r_kjha@yahoo.com>
2022-01-24 12:15:00 +0530

    Calculate wages for a month assuming 20 working days in a month

--- EmpWageCalc.js
+++ EmpWageCalc.js
@@ -6,6 +6,7 @@ const NO_WORK = 0;
 const PART_TIME_WORK = 4;
 const FULL_TIME_WORK = 8;
 const PER_HR_WAGE = 20;
+const WORKING_DAYS_IN_MONTH = 20;
 
 /**
  * UC 1: Check if employee is present or not
@@ -23,24 +24,23 @@ console.log("If employee present today: " + employeePresent);
  */
 
 // To get a random number out of 1, 2 or 3
-let workNoOrPartOrFullTime = Math.ceil((Math.random()*10)/3);
-let empWageToday = 0;
+{
+    let workNoOrPartOrFullTime = Math.ceil((Math.random()*10)/3);
+    let empWageToday = 0;
 
-switch(workNoOrPartOrFullTime){
-    case(1):
-    empWageToday = NO_WORK* PER_HR_WAGE;
-    break;
-    case(2):
-    empWageToday = PART_TIME_WORK*PER_HR_WAGE;
-    break;
-    case(3):
-    empWageToday = FULL_TIME_WORK*PER_HR_WAGE;
-    break;
+    switch(workNoOrPartOrFullTime){
+        case(1):
+        empWageToday = NO_WORK* PER_HR_WAGE;
+        break;
+        case(2):
+        empWageToday = PART_TIME_WORK*PER_HR_WAGE;
+        break;
+        case(3):
+        empWageToday = FULL_TIME_WORK*PER_HR_WAGE;
+        break;
+    }
+    console.log("Wage of employee today: $"+ empWageToday);
 }
-console.log("Wage of employee today: $"+ empWageToday);
-
-
-
 
 /**
  * UC 3: Write function to get work hours
@@ -57,9 +57,19 @@ function getWorkHours(){
     }else if(randomHours == 2){
         hours_worked = 8;
     }
-    console.log("Hours worked by employee today = "+ hours_worked);
     return hours_worked;
 }
-getWorkHours();
+console.log("Hours worked by employee today = "+ getWorkHours());
 
-
+/**
+ * UC 4: Calculate wages for a month assuming 20 working days in a month
+ */
+{
+    let days = 0;
+    let wages_of_month = 0;
+    while(days < WORKING_DAYS_IN_MONTH){
+        wages_of_month += PER_HR_WAGE*getWorkHours(); 
+        days++;
+    }
+    console.log("The wage of the employee in this month = "+ wages_of_month);
+}
