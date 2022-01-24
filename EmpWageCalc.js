43077c0689d6edd394458391e49b2b4470c941ed
rksgr <r_kjha@yahoo.com>
2022-01-24 11:59:52 +0530

    Refactored code: Written function to calculate hours worked in a given day

--- EmpWageCalc.js
+++ EmpWageCalc.js
@@ -10,42 +10,56 @@ const PER_HR_WAGE = 20;
 /**
  * UC 1: Check if employee is present or not
  */
- function checkEmployeeAttendance(){
-    let employeePresent = false;
-    if(Math.random() > 0.5){
-        employeePresent = true;
-    }else if(Math.random() <= 0.5){
-        employeePresent = false;
-    }
-    console.log("If employee present today: "+employeePresent);
-    return employeePresent;
+let employeePresent = false;
+if(Math.random() > 0.5){
+    employeePresent = true;
+}else if(Math.random() <= 0.5){
+    employeePresent = false;
 }
-checkEmployeeAttendance();
+console.log("If employee present today: " + employeePresent);
 
 /**
  * UC 2: Calculate daily employee wage based on part time or full time work using switch case 
  */
 
- function calculateDailyWage(){
+// To get a random number out of 1, 2 or 3
+let workNoOrPartOrFullTime = Math.ceil((Math.random()*10)/3);
+let empWageToday = 0;
 
-    // To get a random number out of 1, 2 or 3
-     let workNoOrPartOrFullTime = Math.ceil((Math.random()*10)/3);
-     let empWageToday = 0;
-
-     switch(workNoOrPartOrFullTime){
-         case(1):
-         empWageToday = NO_WORK* PER_HR_WAGE;
-         break;
-         case(2):
-         empWageToday = PART_TIME_WORK*PER_HR_WAGE;
-         break;
-         case(3):
-         empWageToday = FULL_TIME_WORK*PER_HR_WAGE;
-         break;
-     }
-     console.log("Wage of employee today: $"+ empWageToday);
-     return empWageToday;
+switch(workNoOrPartOrFullTime){
+    case(1):
+    empWageToday = NO_WORK* PER_HR_WAGE;
+    break;
+    case(2):
+    empWageToday = PART_TIME_WORK*PER_HR_WAGE;
+    break;
+    case(3):
+    empWageToday = FULL_TIME_WORK*PER_HR_WAGE;
+    break;
 }
-calculateDailyWage();
+console.log("Wage of employee today: $"+ empWageToday);
+
+
+
+
+/**
+ * UC 3: Write function to get work hours
+ * @return : Hours worked by the employee in a given day
+ */
+function getWorkHours(){
+    let randomHours = Math.floor((Math.random()*10)/3);
+    let hours_worked = 0;
+
+    if(randomHours == 0){
+        hours_worked = 0;
+    }else if(randomHours == 1){
+        hours_worked = 4;
+    }else if(randomHours == 2){
+        hours_worked = 8;
+    }
+    console.log("Hours worked by employee today = "+ hours_worked);
+    return hours_worked;
+}
+getWorkHours();
 
 
