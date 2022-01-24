ef2eebe4886d0d5d7bb260bc5ddd77e3b49ea4df
rksgr <r_kjha@yahoo.com>
2022-01-24 11:07:00 +0530

    Added: Method to check if employee present or not

--- EmpWageCalc.js
+++ EmpWageCalc.js
@@ -2,3 +2,17 @@ console.log("Hi All!\n"+
 "Welcome to Bridgelabz!\n"+
 "Today we shall calculate Wage of employee depending on work done by him.");
 
+/**
+ * UC 1: Check if employee is present or not
+ */
+ function checkEmployeeAttendance(){
+    let employeePresent = false;
+    if(Math.random() > 0.5){
+        employeePresent = true;
+    }else if(Math.random() <= 0.5){
+        employeePresent = false;
+    }
+    console.log("If employee present today: "+employeePresent);
+    return employeePresent;
+}
+checkEmployeeAttendance();
